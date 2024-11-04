f4005c85a60f672e77901f38fc386e59a1744cef
sajjadbhuiyan <sajjadbhuiyan2018@gmail.com>
2024-11-04 16:10:57 +0000

    update work experience

--- src/components/Items/Experience.jsx
+++ src/components/Items/Experience.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Experience({ experience: { year, degree, content, company, tasks, city, country, company_website, company_mail, sector, judged_link, judged_access} }) {
+function Experience({ experience: { year, degree, content, company, tasks, city, country, company_website, company_mail, sector, judged_link, judged_access, skills} }) {
   return (
     <div className="entry">
       <div className="title">
@@ -20,7 +20,7 @@ function Experience({ experience: { year, degree, content, company, tasks, city,
         <div className="flex">
           {company_website && company_website && (
             <>
-              <a href={company_website}><i className="icon-globe"></i> {company_website} </a>    <span className="ml-2">
+              <a href={company_website} target="WhatsAble"><i className="icon-globe"></i> {company_website} </a>    <span className="ml-2">
                 <i className="icon-envelope"></i> 
                 <a href={`mailto:${company_mail}`} className="ml-1">
                   {company_mail}
@@ -33,10 +33,29 @@ function Experience({ experience: { year, degree, content, company, tasks, city,
         </div>
         <br></br>
         <p><b>About the {company}:</b></p>
-        <p>{content}</p>
+        <p>Sector: {sector}</p>
+        <br></br>
+        <p dangerouslySetInnerHTML={{ __html: content }}/>
+        <br></br>
+        <p><b>Responsibilities:</b></p>
         <ul className="task-list mt-2 pl-4">
           {tasks?.map((task, index) => (
-            <li key={index} className="task-item">{task}</li>
+            <li
+              key={index}
+              className="task-item"
+              dangerouslySetInnerHTML={{ __html: task }}
+            />
+          ))}
+        </ul>
+
+        <p><b>Developing Skills:</b></p>
+        <ul className="task-list mt-2 pl-4">
+          {skills?.map((skill, index) => (
+            <li 
+            key={index} 
+            className="task-item"
+            dangerouslySetInnerHTML={{ __html: skill }}
+            />
           ))}
         </ul>
       </div>
--- src/components/Sections/Experiences.jsx
+++ src/components/Sections/Experiences.jsx
@@ -17,49 +17,152 @@ const experiencesData = [
     content:
       "WhatsAble, Unlike traditional setups, which can require a minimum of seven days for non-technical users to configure the Facebook business profile and WhatsApp Business API, our applications offer a streamlined experience. With WhatsAble, users can send messages and track their statuses—such as delivered, read, replied, unsubscribed, and more—in just six minutes through API Key.",
 
-    tasks: ["Collaborated with the development team, working 48 hours per week.", 
-      "Focused on frontend development using Next.js 14 and React.",  "The Notifier by WhatsAble dashboard is built with Next.js 14, while the WhatsAble dashboard uses Next.js 12.", 
-      "Notifier by WhatsAble is a platform that allows users to set up the WhatsApp Business API within 6 minutes.", "From logging into the system to starting messaging, the entire process is streamlined for quick access. Users can utilize an API key to integrate our services. The system is built with Node.js, giving me extensive experience in API development.",
-      "Managed and maintained the backend and databases for both WhatsAble and Notifier by WhatsAble SAAS platforms.",
-      "Applied optimal data structures and algorithms to align with business requirements.",
+    tasks: ["Collaborated with the development team, working 48 hours per week.", "Primary contributor on the development team responsible for building the backend API for Notifier by WhatsAble using Node.js.", "Designing architecture, implementing business logic, structuring data, and managing the database of WhatsAble and Notifier by WhatsAble SAAS", "The <a href='https://notifier.whatsable.app/' target='_blank'>Notifier by WhatsAble</a> dashboard is built with Next.js 14, while the <a href='https://dashboard.whatsable.app/' target='_blank'>WhatsAble Dashboard </a> uses Next.js 12.", "Managed and maintained the backend and databases for both WhatsAble and Notifier by WhatsAble SAAS platforms.", "Always applied optimal data structures and algorithms to align with business requirements.",
       "Developed new features and enhancements.",
-      "Diagnosed and resolved bugs to ensure smooth system operation."]
-  },
-  {
-    id: 1,
-    year: "2020 - Present",
-    degree: "Web Design and Development",
-    content:
-      "This is my patience place. I have 2 years of experience in a different place which was a tiny or well-known place like the Bangladesh Air Force. Mainly I am self-employed.",
-  },
-  {
-    id: 1,
-    year: "2020 - Present",
-    degree: "Web Design and Development",
-    content:
-      "This is my patience place. I have 2 years of experience in a different place which was a tiny or well-known place like the Bangladesh Air Force. Mainly I am self-employed.",
-  },
-  {
-    id: 1,
-    year: "2020 - Present",
-    degree: "Web Design and Development",
-    content:
-      "This is my patience place. I have 2 years of experience in a different place which was a tiny or well-known place like the Bangladesh Air Force. Mainly I am self-employed.",
+      "Diagnosed and resolved bugs to ensure smooth system operation."],
+
+      skills: [
+        "Stay current with fast-growing technologies and AI",
+        "Microservices architecture. In WhatsAble, attachments (files) are managed by a separate API that I created using Xano's AWS S3.",
+        "Improved skills in Node.js, TypeScript and adapting to new versions like Next.js 12 to Next.js 14",
+        "Supabase proficiency",
+        "Business logic-driven API development",
+        "Payment gateway integration with Stripe and Square",
+        "WhatsApp Business API expertise",
+        "Enhanced documentation reading skills",
+        "Automation integration in Make.com and Zapier",
+        "Vercel experience",
+        "Railway deployment skills",
+        "Advanced Git and GitHub knowledge",
+        "Basic AWS understanding"
+    ]
+    
   },
   {
     id: 2,
-    year: "2021 - 2021",
-    degree: "Graphics Designer at Volunteer for Bangladesh",
+    year: "2022 Mar - 2020 Dec",
+    degree: "MIS Officer (Hybrid)",
+    company: "Center for People and Environ (CPE)",
+    city: "Dhaka",
+    country: "Bangladesh",
+    company_website: "https://cpe-bd.org",
+    company_mail: "info@cpe-bd.org",
+    sector: "Research consulting firm",
+    judged_link: "https://www.whatsable.app",
+    judged_access: ["mail: sajjad@gmail.com", "password: test123456"],
     content:
-      "This job was a social collaboration with experience of awareness and noble task. I did Graphics Banner on the Facebook page and there any need.",
+      "Centre for People & Environ (CPE) A hub for scientists, researchers, professionals, academicians and people to work for sustainable development. The organization provides research and capacity building services, science-policy integration at local, regional and national level related to: Climate Resilient Development, Climate Displacement Solution, Environmental Management, Trade Justice and Data Science",
+
+      tasks: [
+        "Oversee all IT-related responsibilities, working 30 hours each week.",
+        "Build their website on a low budget using WordPress.",
+        "Handle the database for web content management.",
+        "Upload content to the website.",
+        "Create vector graphics and perform photo editing.",
+        "Implement a task management tool.",
+        "Provide IT support to the workforce."
+    ],    
+
+    skills: [
+      "Created two websites (an official site and a NEWS portal called poribeshprotibesh). I believe they have altered their <a href='https://cpe-bd.org/poribesh-protibesh-issue-1/'>poribesh-protibesh</a> business model.",
+      "The official website was built using WordPress, where I gained insights into the data structures used in websites and web applications.",
+      "Learned to manage websites, domains, and hosting in a cost-effective manner.",
+      "Acquired skills in theme development.",
+      "I became proficient in various aspects of WordPress.",
+      "Familiar with Bangladeshi payment gateways like SSLCOMMERZ, Bkash, and Nogod.",
+      "Developed strong communication skills through Twilio setup.",
+      "Experienced in photo editing tools such as Photoshop, GIMP, Photopea, and others.",
+      "Skilled in vector graphics, having completed over 100 vector designs using Adobe Illustrator and Inkscape."
+  ]
+    
   },
   {
     id: 3,
-    year: "2020 - 2020",
-    degree: "Customer Care Executive at FIFO Tech",
+    year: "2023 Jan - Present",
+    degree: "MIS Officer (Contract)",
+    company: "Center for Climate Intelligence (CCI)",
+    city: "Dhaka",
+    country: "Bangladesh",
+    company_website: "https://climateintelligence.netlify.app",
+    company_mail: "info@climateintelligence.center",
+    sector: "Data Science",
+    judged_link: "https://www.whatsable.app",
+    judged_access: ["mail: sajjad@gmail.com", "password: test123456"],
     content:
-      "This is my first corporate job. I have experienced corporate manner, etiquette and communication from there.",
+      "We are a team of passionate individuals who are committed to using technology and data to tackle the challenges of climate change. We are a diverse group of experts with backgrounds in climate science, data science, machine learning, and artificial intelligence, among other fields. <a href='https://climateintelligence.netlify.app/about/' target='_blank'>more...</a>",
+
+   
+    tasks: [
+        "When need I support them",
+        "Build their website with next js 13",
+        "Creating Backend with node js",
+        "Provide IT support to the workforce."
+    ],    
+
+    skills: [
+      "Next js",
+      "I am gaining knowledge of data science",
+      "Communication Skill",
+  ]
+  },
+  {
+    id: 4,
+    year: "2021 Jun - 2021 Dec",
+    degree: "Graphics Designer at Volunteer for Bangladesh",
+    company: "Volunteer for Bangladesh",
+    city: "Dhaka",
+    country: "Bangladesh",
+    company_website: "https://vbd.com.bd/",
+    company_mail: "info@vbd.com.bd",
+    sector: "Volunteer organization of JAAGO Foundation",
+    judged_link: "https://www.whatsable.app",
+    judged_access: ["mail: sajjad@gmail.com", "password: test123456"],
+    content:
+      "Volunteer for Bangladesh is a youth wing of JAAGO Foundation, established in 2011 with support of U.S Embassy to Bangladesh. Its goal is to empower young people in Bangladesh to volunteer and develop leadership skills to make positive change in their communities. It provides platform for community service, volunteer work, training, and mentorship opportunities.",
+
+    tasks: [
+        "Vector Graphics",
+        "Photo Editing",
+        "Group collaboration"
+    ],    
+
+    skills: [
+      "Vector Graphics",
+      "Photo Editing",
+      "MS Office Skill",
+  ]
+
+  },
+  {
+    id: 5,
+    year: "2020 Jun - 2020",
+    degree: "Customer Care Executive (Remote)",
+    company: "FIFO Tech",
+    city: "Dhaka",
+    country: "Bangladesh",
+    company_website: "https://fifo-tech.com/",
+    company_mail: "info@vbd.com.bd",
+    sector: "BPO service provider",
+    judged_link: "https://www.whatsable.app",
+    judged_access: ["mail: sajjad@gmail.com", "password: test123456"],
+    content:
+      "FIFOTech is one of the leading BPO service providers in Bangladesh. We provide a full range of IT/ITES services considering customer satisfaction. <a href='https://fifo-tech.com/about/' target='_blank'>more...</a>",
+
+    tasks: [
+
+        "Ingoing and outgoing call",
+        "Customer Support",
+        "Team meeting and everyday update"
+    ],    
+
+    skills: [
+      "First corporate experience",
+      "Working 48 hours per week",
+      "Handle customer with cool mind",
+  ]
   }
+
+  
 ];
 
 function Experiences() {
